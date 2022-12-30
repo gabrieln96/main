c2c6fb2af93bed9c92e8722cd88632c05eee68b0
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-30 20:54:25 -0300

    about me

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about2.jpg'
+
+import {BsAward, BsFolderCheck} from 'react-icons/bs'
+import {HiOutlineUserGroup} from 'react-icons/hi'
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Quem sou eu?</h5>
+      <h2>Sobre mim</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+            <div className="about_me-image">
+              <img src={ME} alt="About me" />
+            </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+                <BsAward className='about__icon'/>
+              <h5>Experiência</h5>
+              <small>1+ Anos</small>
+            </article>
+            <article className='about__card'>
+                <HiOutlineUserGroup className='about__icon'/>
+              <h5>Clientes</h5>
+              <small>...</small>
+            </article>
+            <article className='about__card'>
+                <BsFolderCheck className='about__icon'/>
+              <h5>Projetos</h5>
+              <small>5+ Completos</small>
+            </article>
+          </div>
+        </div>
+      </div>
+
+    </section>
   )
 }
 
