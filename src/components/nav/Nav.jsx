86b526109d1a4844b42d630919b00cf01a8ef405
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-12-30 13:40:33 -0300

    hook do nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 
 import './nav.css'
 import {GrHomeRounded, GrUser, GrBook, GrServicePlay, GrContactInfo} from 'react-icons/gr'
 
 const Nav = () => {
+
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
     <nav>
-      <a href="#" className='active'><GrHomeRounded /></a>
-      <a href="#about"><GrUser /></a>
-      <a href="#experience"><GrBook /></a>
-      <a href="#services"><GrServicePlay /></a>
-      <a href="#contact"><GrContactInfo /></a>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><GrHomeRounded /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')}  className={activeNav === '#about' ? 'active' : ''} ><GrUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')}  className={activeNav === '#experience' ? 'active' : ''}><GrBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')}  className={activeNav === '#services' ? 'active' : ''}><GrServicePlay /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')}  className={activeNav === '#contact' ? 'active' : ''}><GrContactInfo /></a>
     </nav>
   )
 }
