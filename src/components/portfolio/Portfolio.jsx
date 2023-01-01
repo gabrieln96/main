008e04f9d86d64f41ce7613fb08fe86534cbcfc0
gabrieln96 <gabrielaraujo.n96@gmail.com>
2023-01-01 20:24:23 -0300

    testimonials jsx/css

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
+import IMG1 from '../../assets/portfolio1.png'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'PhotoGrapys - Portfolio de fotos!',
+    github: 'https://github.com/gabrieln96/photograpys',
+    demo: 'https://photograpys.web.app/'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Titulo 2',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Titulo 3',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Titulo 4',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/'
+  },
+]
 
 
 const Portfolio = () => {
@@ -14,48 +45,24 @@ const Portfolio = () => {
     <section id='portfolio'>
       <h5>Meus projetos recentes</h5>
       <h2>Portfólio</h2>
-
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="img1" />
-            </div>
-            <h3>Titulo do portfolio</h3>
-           <div className="portfolio__item-cta">
-           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-           </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="img2" />
-            </div>
-            <h3>Titulo do portfolio</h3>
-           <div className="portfolio__item-cta">
-           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-           </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="img3" />
-            </div>
-            <h3>Titulo do portfolio</h3>
-           <div className="portfolio__item-cta">
-           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-           </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="img4" />
-            </div>
-            <h3>Titulo do portfolio</h3>
-           <div className="portfolio__item-cta">
-           <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://github.com" className='btn btn-primary' target='_blank'>Live Demo</a>
-           </div>
-        </article>
+      {
+        data.map(({id, image, title, github, demo}) => {
+          return (
+            <article key={id}className='portfolio__item'>
+            <div className="portfolio__item-image">
+              <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+             <div className="portfolio__item-cta">
+             <a href={github} className='btn' target='_blank'>Github</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+             </div>
+          </article>
+          )
+        })
+      }
+    
         
       </div>
     </section>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,9 +2,61 @@ import React from 'react'
 
 import './testimonials.css'
 
+import AVR1 from '../../assets/avatar1.jpg'
+import AVR2 from '../../assets/avatar2.jpg'
+import AVR3 from '../../assets/avatar3.jpg'
+import AVR4 from '../../assets/avatar4.jpg'
+
+
 const Testimonials = () => {
   return (
-    <section id='testimonials'>Testimonials</section>
+    <section id='testimonials'>
+      <h5>Review de clientes</h5>
+      <h2>Testemunhos</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+            <div className="client__avatar">
+                <img src={AVR1} alt="Avatar Um" />
+                <h5 className='client__name'>Teste teste</h5>
+            </div>
+            <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nihil, culpa debitis itaque atque tempore odio sit nisi minima illo, magni recusandae earum nulla nesciunt saepe cum velit quia distinctio?
+                </small>
+          </article>
+          
+          <article className="testimonial">
+            <div className="client__avatar">
+                <img src={AVR1} alt="Avatar Um" />
+                <h5 className='client__name'>Teste teste</h5>
+            </div>
+            <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nihil, culpa debitis itaque atque tempore odio sit nisi minima illo, magni recusandae earum nulla nesciunt saepe cum velit quia distinctio?
+                </small>
+          </article>
+
+          <article className="testimonial">
+            <div className="client__avatar">
+                <img src={AVR1} alt="Avatar Um" />
+                <h5 className='client__name'>Teste teste</h5>
+            </div>
+            <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nihil, culpa debitis itaque atque tempore odio sit nisi minima illo, magni recusandae earum nulla nesciunt saepe cum velit quia distinctio?
+                </small>
+          </article>
+          
+          <article className="testimonial">
+            <div className="client__avatar">
+                <img src={AVR1} alt="Avatar Um" />
+                <h5 className='client__name'>Teste teste</h5>
+            </div>
+            <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nihil, culpa debitis itaque atque tempore odio sit nisi minima illo, magni recusandae earum nulla nesciunt saepe cum velit quia distinctio?
+                </small>
+          </article>
+          
+        </div>
+    </section>
   )
 }
 
